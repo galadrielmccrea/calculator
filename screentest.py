956a667779669586600f3f37f930c16dd590b414
Galadriel <52978451+galadrielmccrea@users.noreply.github.com>
2023-02-15 20:41:02 -0500

    autopush

--- screentest.py
+++ screentest.py
@@ -14,17 +14,17 @@ Author(s): Melissa LeBlanc-Williams for Adafruit Industries
 import digitalio
 import board
 from PIL import Image, ImageDraw
-from adafruit_rgb_display import ili9341
-from adafruit_rgb_display import st7789  # pylint: disable=unused-import
+#from adafruit_rgb_display import ili9341
+#from adafruit_rgb_display import st7789  # pylint: disable=unused-import
 from adafruit_rgb_display import hx8357  # pylint: disable=unused-import
-from adafruit_rgb_display import st7735  # pylint: disable=unused-import
-from adafruit_rgb_display import ssd1351  # pylint: disable=unused-import
-from adafruit_rgb_display import ssd1331  # pylint: disable=unused-import
+#from adafruit_rgb_display import st7735  # pylint: disable=unused-import
+#from adafruit_rgb_display import ssd1351  # pylint: disable=unused-import
+#from adafruit_rgb_display import ssd1331  # pylint: disable=unused-import
 
 # Configuration for CS and DC pins (these are PiTFT defaults):
-cs_pin = digitalio.DigitalInOut(board.CE0)
+cs_pin = digitalio.DigitalInOut(board.D8)
 dc_pin = digitalio.DigitalInOut(board.D25)
-reset_pin = digitalio.DigitalInOut(board.D24)
+reset_pin = digitalio.DigitalInOut(board.D4)
 
 # Config for display baudrate (default max is 24mhz):
 BAUDRATE = 24000000
@@ -39,7 +39,7 @@ spi = board.SPI()
 # disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
 # disp = st7789.ST7789(spi, rotation=90, width=172, height=320, x_offset=34, # 1.47" ST7789
 # disp = st7789.ST7789(spi, rotation=270, width=170, height=320, x_offset=35, # 1.9" ST7789
-# disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
 # disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
 # disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
 # disp = st7735.ST7735R(spi, rotation=90, bgr=True, width=80,       # 0.96" MiniTFT Rev A ST7735R
